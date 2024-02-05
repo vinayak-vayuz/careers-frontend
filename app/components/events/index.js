90ec3d938e6d69edc62be5fd2193abef60010b90
vinayaktripathi-in <vinayaktripathi@hotmail.com>
2024-02-05 11:07:56 +0530

    initial commit

--- app/components/events/index.js
+++ app/components/events/index.js
@@ -2,12 +2,12 @@ import Image from "next/image";
 
 const eventsData = [
   { imageSrc: "/images/events/flutter_nest.png", className: "absolute top-0 left-0", width: 332, height: 174 },
-  { imageSrc: "/images/events/birthday_matinee.png", className: "absolute top-[15%] left-[25%]", width: 287, height: 231 },
+  { imageSrc: "/images/events/birthday_matinee.png", className: "absolute top-[10%] left-[25%]", width: 287, height: 231 },
   { imageSrc: "/images/events/blank_event_01.png", className: "absolute top-[10%] left-[60%] scale-50", width: 144, height: 117 },
   { imageSrc: "/images/events/demo_saturday.png", className: "absolute top-[2%] right-[0%]", width: 337, height: 294 },
   { imageSrc: "/images/events/horcrux.png", className: "absolute top-[50%] left-[10%]", width: 396, height: 320 },
   { imageSrc: "/images/events/drishtikone.png", className: "absolute top-[40%] right-[20%]", width: 330, height: 330 },
-  { imageSrc: "/images/events/blank_event_02.png", className: "absolute bottom-[0%] left-[0%] w-28", width: 221, height: 185 },
+  { imageSrc: "/images/events/blank_event_02.png", className: "absolute bottom-[0%] left-[0%] w-24", width: 221, height: 185 },
   { imageSrc: "/images/events/blank_event_03.png", className: "absolute bottom-[0%] right-[50%] w-32", width: 224, height: 183 },
   { imageSrc: "/images/events/developer_meet.png", className: "absolute bottom-[0%] right-[2%]", width: 206, height: 165 },
 ];
