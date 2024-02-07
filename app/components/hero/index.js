92bccd1a1211c76953b2a43f3705003149025551
vinayaktripathi-in <vinayaktripathi@hotmail.com>
2024-02-07 16:35:46 +0530

    initial commit

--- app/components/hero/index.js
+++ app/components/hero/index.js
@@ -18,11 +18,11 @@ const Hero = ({ id }) => {
       id={id}
       className={`${bungee.className} w-full min-h-screen sm:min-h-[calc(100vh-100px)] relative flex flex-col justify-center items-center text-center`}
     >
-      <h1 className="text-[28px] xl:text-[85px] xl:leading-[100px] flex justify-center items-center gap-4">
+      <h1 className="text-[28px] sm:text-[40px] md:text-[60px] xl:text-[85px] xl:leading-[100px] flex justify-center items-center gap-4">
         <span className="text-white">We Are</span>
         <span className="font-outline-2">People</span>
       </h1>
-      <h1 className="text-[28px] xl:text-[85px] xl:leading-[100px] flex justify-center items-center gap-4">
+      <h1 className="text-[28px] sm:text-[40px] md:text-[60px] xl:text-[85px] xl:leading-[100px] flex justify-center items-center gap-4">
         <div>
           <span className="text-white">W</span>
           <span className="font-outline-2">ho</span>
@@ -36,7 +36,7 @@ const Hero = ({ id }) => {
           <span className="font-outline-2">bout</span>
         </div>
       </h1>
-      <h1 className="text-[28px] xl:text-[85px] xl:leading-[100px]">
+      <h1 className="text-[28px] sm:text-[40px] md:text-[60px] xl:text-[85px] xl:leading-[100px]">
         <span className="text-white">Y</span>
         <span className="font-outline-2">o</span>
         <span className="text-white">u</span>
