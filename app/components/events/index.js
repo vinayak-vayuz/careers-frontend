b25f6d465d92c6525e8fd411d1796f8e975b840f
vinayaktripathi-in <vinayaktripathi@hotmail.com>
2024-02-23 15:29:22 +0530

    initial commit

--- app/components/events/index.js
+++ app/components/events/index.js
@@ -1,8 +1,8 @@
 import Image from "next/image";
-import { useRef, useState } from "react";
-import { Bungee } from "next/font/google";
 import { motion } from "framer-motion";
+import { Bungee } from "next/font/google";
 import { eventsData } from "./eventsData";
+import { useState, useEffect, useRef } from "react";
 import { useOutsideClick } from "@/app/functions/outsideClick";
 
 const bungee = Bungee({
@@ -32,12 +32,6 @@ const Expand = ({
     setSelected(false);
   };
   useOutsideClick(menuRef, closeMenu);
-
-  // const spring = {
-  //   type: "spring",
-  //   damping: 10,
-  //   stiffness: 100,
-  // };
 
   return (
     <>
@@ -69,13 +63,85 @@ const Expand = ({
 };
 
 const Events = () => {
+  const [lastPos, setLastPos] = useState(0);
+  const perspective = 300;
+  const zSpacing = -11800;
+  const numFrames = 1;
+
+  const zVals = Array.from(
+    { length: numFrames },
+    (_, i) => (numFrames - i) * zSpacing
+  );
+
+  const frames = Array.from({ length: numFrames }, () => useRef(null));
+  const scrollMsgRef = useRef(null);
+
+  const handleScroll = () => {
+    const top = window.scrollY || document.documentElement.scrollTop;
+    const delta = lastPos - top;
+    setLastPos(top);
+
+    zVals.forEach((zVal, i) => {
+      const newZVal = zVal + delta * -1.5;
+      const frame = frames[i].current;
+      const transform = `translateZ(${newZVal}px)`;
+      const opacity =
+        newZVal < 200
+          ? 1
+          : 1 - parseInt(((newZVal - 200) / (perspective - 200)) * 10) / 10;
+      const display = newZVal > perspective ? "none" : "block";
+
+      frame.style.transform = transform;
+      frame.style.opacity = opacity;
+      frame.style.display = display;
+
+      if (zVals[numFrames - 1] > 200 && scrollMsgRef.current) {
+        scrollMsgRef.current.style.display = "none";
+      }
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <>
+      <div className="hidden md:block body-content">
+        <div id="content">
+          <div id="viewport">
+            {frames.map((frame, index) => (
+              <div
+                key={index}
+                className={`frame ${index % 2 === 0 ? "box" : "text"}`}
+                style={{ transform: `translateZ(${zVals[index]}px)` }}
+                ref={frame}
+              >
+                {eventsData.map((data, index) => (
+                  <Expand
+                    key={index}
+                    className={`${data.className} w-52`}
+                    imageSrc={data.imageSrc}
+                    width={data.width}
+                    height={data.height}
+                    alt={data.alt}
+                    title={data.title}
+                    description={data.description}
+                  />
+                ))}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
       <motion.section
         transition={{ duration: 1 }}
         initial={{ opacity: 0 }}
         whileInView={{ opacity: 1 }}
-        className="bg-[#141414] px-4 py-10 relative w-full flex flex-nowrap justify-center items-center gap-2 overflow-x-auto no-scrollbar"
+        className="flex md:hidden bg-[#141414] px-4 py-10 relative w-full min-h-auto md:min-h-screen flex-nowrap justify-center items-center gap-2 overflow-x-auto no-scrollbar"
       >
         {eventsData.map((data, index) => (
           <Expand
--- app/components/test/index.js
+++ app/components/test/index.js
@@ -1,162 +1,163 @@
-import React, { useState, useEffect, useRef } from "react";
-import { eventsData } from "../events/eventsData";
-import { Bungee } from "next/font/google";
-import { useOutsideClick } from "@/app/functions/outsideClick";
-import Image from "next/image";
+// import React, { useState, useEffect, useRef } from "react";
+// import { eventsData } from "../events/eventsData";
+// import { Bungee } from "next/font/google";
+// import { motion } from "framer-motion";
+// import { useOutsideClick } from "@/app/functions/outsideClick";
+// import Image from "next/image";
 
-const bungee = Bungee({
-  weight: "400",
-  subsets: ["latin"],
-});
+// const bungee = Bungee({
+//   weight: "400",
+//   subsets: ["latin"],
+// });
 
-const Expand = ({
-  className,
-  imageSrc,
-  eImageSrc,
-  width,
-  height,
-  alt,
-  title,
-  description,
-}) => {
-  const [selected, setSelected] = useState(false);
+// const Expand = ({
+//   className,
+//   imageSrc,
+//   eImageSrc,
+//   width,
+//   height,
+//   alt,
+//   title,
+//   description,
+// }) => {
+//   const [selected, setSelected] = useState(false);
 
-  const handleClick = () => {
-    setSelected(!selected);
-  };
+//   const handleClick = () => {
+//     setSelected(!selected);
+//   };
 
-  // Handling outside click events
-  const menuRef = useRef(null);
-  const closeMenu = () => {
-    setSelected(false);
-  };
-  useOutsideClick(menuRef, closeMenu);
+//   // Handling outside click events
+//   const menuRef = useRef(null);
+//   const closeMenu = () => {
+//     setSelected(false);
+//   };
+//   useOutsideClick(menuRef, closeMenu);
 
-  // const spring = {
-  //   type: "spring",
-  //   damping: 10,
-  //   stiffness: 100,
-  // };
+//   return (
+//     <>
+//       <div ref={menuRef} onClick={handleClick}>
+//         <Image
+//           className={`${className} w-52`}
+//           src={imageSrc}
+//           width={width}
+//           height={height}
+//           alt={alt}
+//         />
+//         {selected ? (
+//           <div
+//             // layout
+//             // transition={{ duration: 1 }}
+//             className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10 p-4 bg-black rounded-xl"
+//           >
+//             <h1 className={`${bungee.className} text-2xl text-[#A35DFF]`}>
+//               {title}
+//             </h1>
+//             <h1 className="text-white">{description}</h1>
+//           </div>
+//         ) : (
+//           <></>
+//         )}
+//       </div>
+//     </>
+//   );
+// };
 
-  return (
-    <>
-      <div ref={menuRef} onClick={handleClick}>
-        <Image
-          className={`${className} w-52`}
-          src={imageSrc}
-          width={width}
-          height={height}
-          alt={alt}
-        />
-        {selected ? (
-          <div
-            // layout
-            // transition={{ duration: 1 }}
-            className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10 p-4 bg-black rounded-xl"
-          >
-            <h1 className={`${bungee.className} text-2xl text-[#A35DFF]`}>
-              {title}
-            </h1>
-            <h1 className="text-white">{description}</h1>
-          </div>
-        ) : (
-          <></>
-        )}
-      </div>
-    </>
-  );
-};
+// const Test = () => {
+//   const [lastPos, setLastPos] = useState(0);
+//   const perspective = 300;
+//   const zSpacing = -1500;
+//   const numFrames = 1;
 
-const Test = () => {
-  const [lastPos, setLastPos] = useState(0);
-  const perspective = 300;
-  const zSpacing = -1500;
-  const numFrames = 1;
+//   const zVals = Array.from(
+//     { length: numFrames },
+//     (_, i) => (numFrames - i) * zSpacing
+//   );
 
-  const zVals = Array.from(
-    { length: numFrames },
-    (_, i) => (numFrames - i) * zSpacing
-  );
+//   const frames = Array.from({ length: numFrames }, () => useRef(null));
+//   const scrollMsgRef = useRef(null);
 
-  const frames = Array.from({ length: numFrames }, () => useRef(null));
-  const scrollMsgRef = useRef(null);
+//   const handleScroll = () => {
+//     const top = window.scrollY || document.documentElement.scrollTop;
+//     const delta = lastPos - top;
+//     setLastPos(top);
 
-  const handleScroll = () => {
-    const top = window.scrollY || document.documentElement.scrollTop;
-    const delta = lastPos - top;
-    setLastPos(top);
+//     zVals.forEach((zVal, i) => {
+//       const newZVal = zVal + delta * -1.5;
+//       const frame = frames[i].current;
+//       const transform = `translateZ(${newZVal}px)`;
+//       const opacity =
+//         newZVal < 200
+//           ? 1
+//           : 1 - parseInt(((newZVal - 200) / (perspective - 200)) * 10) / 10;
+//       const display = newZVal > perspective ? "none" : "block";
 
-    zVals.forEach((zVal, i) => {
-      const newZVal = zVal + delta * -1.5;
-      const frame = frames[i].current;
-      const transform = `translateZ(${newZVal}px)`;
-      const opacity =
-        newZVal < 200
-          ? 1
-          : 1 - parseInt(((newZVal - 200) / (perspective - 200)) * 10) / 10;
-      const display = newZVal > perspective ? "none" : "block";
+//       frame.style.transform = transform;
+//       frame.style.opacity = opacity;
+//       frame.style.display = display;
 
-      frame.style.transform = transform;
-      frame.style.opacity = opacity;
-      frame.style.display = display;
+//       if (zVals[numFrames - 1] > 200 && scrollMsgRef.current) {
+//         scrollMsgRef.current.style.display = "none";
+//       }
+//     });
+//   };
 
-      if (zVals[numFrames - 1] > 200 && scrollMsgRef.current) {
-        scrollMsgRef.current.style.display = "none";
-      }
-    });
-  };
+//   useEffect(() => {
+//     window.addEventListener("scroll", handleScroll);
+//     return () => {
+//       window.removeEventListener("scroll", handleScroll);
+//     };
+//   }, []);
 
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
+//   return (
+//     <>
+//       <div className="hidden md:block body-content">
+//         <div id="content">
+//           <div id="viewport">
+//             {frames.map((frame, index) => (
+//               <div
+//                 key={index}
+//                 className={`frame ${index % 2 === 0 ? "box" : "text"}`}
+//                 style={{ transform: `translateZ(${zVals[index]}px)` }}
+//                 ref={frame}
+//               >
+//                 {eventsData.map((data, index) => (
+//                   <Expand
+//                     key={index}
+//                     className={`${data.className} w-52`}
+//                     imageSrc={data.imageSrc}
+//                     width={data.width}
+//                     height={data.height}
+//                     alt={data.alt}
+//                     title={data.title}
+//                     description={data.description}
+//                   />
+//                 ))}
+//               </div>
+//             ))}
+//           </div>
+//         </div>
+//       </div>
+//       <motion.section
+//         transition={{ duration: 1 }}
+//         initial={{ opacity: 0 }}
+//         whileInView={{ opacity: 1 }}
+//         className="flex md:hidden bg-[#141414] px-4 py-10 relative w-full min-h-auto md:min-h-screen flex-nowrap justify-center items-center gap-2 overflow-x-auto no-scrollbar"
+//       >
+//         {eventsData.map((data, index) => (
+//           <Expand
+//             key={index}
+//             className={`${data.className} min-w-[200px] flex-shrink-0 w-52`}
+//             imageSrc={data.imageSrc}
+//             width={data.width}
+//             height={data.height}
+//             alt={data.alt}
+//             title={data.title}
+//             description={data.description}
+//           />
+//         ))}
+//       </motion.section>
+//     </>
+//   );
+// };
 
-  return (
-    <div className="body-content">
-      <div id="content">
-        <div id="viewport">
-          {/* <div id="instructions-overlay" ref={scrollMsgRef}>
-            <h1>Initial Instructions Overlay</h1>
-            <p>This will go away after you scroll</p>
-            <p>
-              Orig Source Code From{" "}
-              <a href="http://eng.wealthfront.com/2012/03/22/scrolling-z-axis-with-css-3d-transforms/">
-                Matt Baker
-              </a>
-            </p>
-          </div> */}
-
-          {frames.map((frame, index) => (
-            <div
-              key={index}
-              className={`frame ${index % 2 === 0 ? "box" : "text"}`}
-              style={{ transform: `translateZ(${zVals[index]}px)` }}
-              ref={frame}
-            >
-              {/* <h1>{`Frame ${numFrames - index} (${
-                index % 2 === 0 ? "Box" : "Text"
-              })`}</h1> */}
-              {eventsData.map((data, index) => (
-                <Expand
-                  key={index}
-                  className={`${data.className} w-52`}
-                  imageSrc={data.imageSrc}
-                  width={data.width}
-                  height={data.height}
-                  alt={data.alt}
-                  title={data.title}
-                  description={data.description}
-                />
-              ))}
-              {/* {index === 8 && <p>Lorem ipsum</p>} */}
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default Test;
+// export default Test;
